9b252eea98aeaabf2d7421d3c9c3b75f939cc361
gustavogouveiaaa <gustavogouveia39@gmail.com>
2023-09-29 20:47:27 -0300

    Configuração de adicionar-assinatura atualizado com pagina para teste

--- src/index.js
+++ src/index.js
@@ -2,55 +2,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const { PDFDocument, rgb } = require('pdf-lib');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/teste.html'); 
+    res.sendFile(__dirname + '/public/teste.html');
 });
 
 app.post('/adicionar-assinatura', upload.single('pdfFile'), async (req, res) => {
     try {
         const pdfBytes = req.file.buffer;
 
-      
+        // Carrega o PDF original
         const pdfDoc = await PDFDocument.load(pdfBytes);
         const pages = pdfDoc.getPages();
 
-        // define a assinatura(AINDA DECIDIR COMO SERÁ A ASS)
-        const signatureText = 'Assinado por: "olá"';
+        // aqui define o que vai ser assinado.
+        const signatureText = 'Assinado por: olá';
 
-        // adiciona a assinatura
+        //aqui adiciona a assinatura e define o local onde ela fica também
         for (const page of pages) {
             const { width, height } = page.getSize();
             const fontSize = 12;
-            const textWidth = width / 2 - fontSize;
-            const textHeight = 20;
+
+            const textWidth = (width - fontSize * signatureText.length) / 2;
+            const textHeight = (height - fontSize) / 2;
 
             page.drawText(signatureText, {
                 x: textWidth,
                 y: textHeight,
                 size: fontSize,
-                color: rgb(0, 0, 0), 
+                color: rgb(0, 0, 0),
             });
         }
 
-        const pdfBytesModificado = await pdfDoc.save();
+        
+        const modifiedPdfData = await pdfDoc.save({ format: 'binary' });
 
-        console.log('Tamanho do PDF gerado:', pdfBytesModificado.length); // testando
-
-        // Enviar o PDF modificado como resposta
+        
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename="pdf-modificado.pdf"');
-        res.send(pdfBytesModificado);
+
+        
+        res.send(Buffer.from(modifiedPdfData, 'binary'));
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Ocorreu um erro ao adicionar a assinatura ao PDF.' });
